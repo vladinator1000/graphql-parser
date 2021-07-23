800aaf9e1c48428d18a9970e24fc4ba29dccead2
Vlady Veselinov <vladkodmc@gmail.com>
2021-07-23 16:24:11 +0100

    happy lil pyramid

--- src/visitor/visitor.rs
+++ src/visitor/visitor.rs
@@ -56,38 +56,69 @@ pub fn visit<'ast, V: QueryVisitor<'ast, T>, T: Text<'ast>>(
 
 #[cfg(test)]
 mod visitor_tests {
+    use k9::assert_equal;
+
     use super::*;
     use crate::parse_query;
     use crate::query::*;
     use crate::Pos;
 
     #[test]
-    fn allows_editing_a_node_both_on_enter_and_on_leave() {
+    fn allows_node_editing_both_on_enter_and_leave() {
         let ast: Document<&str> =
             parse_query("{ a, b, c { a, b, c } }").expect("Failed to parse query");
 
         struct SelectionSetVisitor<'ast, T: Text<'ast>> {
-            selection_set: &'ast SelectionSet<'ast, T>,
+            selection_sets: Vec<&'ast SelectionSet<'ast, T>>,
         }
 
         impl<'ast, T: Text<'ast>> QueryVisitor<'ast, T> for SelectionSetVisitor<'ast, T> {
             fn enter(&mut self, node: &'ast QueryAstNode<'ast, T>) -> VisitorAction<'ast, T> {
                 match node {
-                    QueryAstNode::OperationDefinition(definition) => {
-                       match definition {
-                           OperationDefinition::SelectionSet(selection_set) => {
-                               self.selection_set = selection_set;
-                               VisitorAction::ReplaceNode(QueryAstNode::SelectionSet(SelectionSet {
-                                   span: (Pos { line: 0, column: 0 }, Pos { line: 0, column: 0 }),
-                                   items: vec![]
-                               }))
-                           },
-                           _ => VisitorAction::NoAction
-                       }
+                    QueryAstNode::OperationDefinition(definition) => match definition {
+                        OperationDefinition::SelectionSet(selection_set) => {
+                            self.selection_sets.push(selection_set);
+
+                            let empty_position = Pos { line: 0, column: 0 };
+                        
+                            let new_field = Field {
+                                position: empty_position,
+                                alias: None,
+                                name: "enter".into(),
+                                arguments:  vec![],
+                                directives: vec![],
+                                selection_set: SelectionSet {
+                                    span: (empty_position, empty_position),
+                                    items: vec![]
+                                }
+                            };
+
+                            let new_selection_set = QueryAstNode::SelectionSet(SelectionSet {
+                                span: (empty_position, empty_position),
+                                items: vec![Selection::Field(new_field)],
+                            });
+
+                            VisitorAction::ReplaceNode(new_selection_set)
+                        }
+                        _ => VisitorAction::NoAction,
                     },
-                    _ => VisitorAction::NoAction
+                    _ => VisitorAction::NoAction,
                 }
             }
+
+            fn leave(&mut self, node: &'ast QueryAstNode<'ast, T>) -> VisitorAction<'ast, T> {
+                VisitorAction::NoAction
+            }
+        }
+
+        let mut selection_set_visitor = SelectionSetVisitor {
+            selection_sets: Vec::new()
+        };
+
+        if let QueryAstNode::Document(edited_document) = visit(&QueryAstNode::Document(ast), &mut selection_set_visitor) {
+            let edited = edited_document.to_string();
+            let expected = "{ a, b, c { enter, enter, enter } }".to_string();
+            assert_equal!(edited, expected);
         }
     }
 }
